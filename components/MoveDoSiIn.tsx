a99f080caf637900ad4b57cff74056263933d37f
JinSeop <turn3361@gmail.com>
2020-08-28 16:48:07 +0900

    :heavy_plus_sign:  도시인 이동페이지 css완성
     -도시인 이동페이지 css 완성

--- components/MoveDoSiIn.tsx
+++ components/MoveDoSiIn.tsx
@@ -1,24 +1,61 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function MoveDoSiIn() {
   return(
-    <View>
-      <View>
-        <Text>DO.SI.IN</Text>
-      </View>
-      <View>
-        <Text>남은 식재료를 도시락 형태로 공유,거래할 수 있는 플랫폼 입니다.</Text>
-      </View>
-      <View>
-        <TouchableOpacity>
-          <Text>DO.SI.IN으로 이동</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>취소</Text>
-        </TouchableOpacity>
+    <View style={style.background}>
+      <View style={style.mainWrapper}>
+        <View style={style.mainTitle}>
+          <Text style={style.mainTitleText}>DO.SI.IN</Text>
+        </View>
+        <View>
+          <Text style={style.bodyText}>남은 식재료를 도시락 형태로 공유,거래할 수 있는 플랫폼 입니다.</Text>
+        </View>
+        <View style={style.buttonWrapper}>
+          <TouchableOpacity style={style.button}>
+            <Text>DO.SI.IN으로 이동</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={style.button}>
+            <Text>취소</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  background:{
+    width:'100%',
+    height:'100%',
+    backgroundColor:'gray',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  mainWrapper:{
+    width:'80%',
+    backgroundColor:'#F3ECA5',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  mainTitle:{
+    marginBottom:10,
+  },
+  mainTitleText:{
+    fontSize:25
+  },
+  bodyText:{
+    fontSize:15,
+    margin:15
+  },
+  buttonWrapper:{
+    flexDirection:'row'
+  },
+  button:{
+    margin:15,
+    padding:10,
+    backgroundColor:'white',
+    borderRadius:5
+  }
+});
