c3e73155cd35ec982ef52d41daf136a130032b24
JinSeop <turn3361@gmail.com>
2020-09-02 11:21:42 +0900

    :heavy_plus_sign: 회원정보 수정 기능 구현

--- components/EditUserInfo.tsx
+++ components/EditUserInfo.tsx
@@ -1,25 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View ,Image, ImageBackground, TextInput,TouchableOpacity } from 'react-native';
 import EditUserInfoImage from '../assets/Mypage.png';
 import WhiteBackgroundImage from '../assets/city_white.jpg';
+import Axios from 'axios';
 
 export default function EditUserInfo(props:any) {
+
+  const [userid,setUserid] = useState<string>('');
+  const [password,setPassword] = useState<string>('');
+  const [checkPassword,setCheckPassword] = useState<string>('');
+  const [email,setEmail] = useState<string>('');
+
+
+  const changeHandler = () => {
+    const checkEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    if (
+      password.length === 0 ||
+      checkPassword.length === 0 ||
+      email.length === 0 ||
+      password !== checkPassword
+    ) {
+      return alert('입력이 잘 못 되었습니다. 다시입력해 주세요');
+    }
+    
+    if (password.length < 8) {
+      return alert('비밀번호를 8자 이상으로 해주세요');
+    }
+
+    if (!checkEmail.test(email)) {
+      return alert('이메일이 잘 못 되었습니다');
+    }
+    if(password === checkPassword){
+      Axios.put('http://52.78.146.191:5000/mypage/setupPut',{
+        email,
+        userid,
+        password
+      })
+        .then(res => {
+          if(res.status === 201){
+            alert('회원정보가 수정되었습니다.');
+            props.navigation.navigate('Mypage');
+          }
+        })
+        .catch(err => console.error(err));
+    }
+    if(password !== checkPassword){
+      return alert('비밀번호가 일치하지 않습니다.');
+    }
+  };
+
+  useEffect(()=> {setUserid(props.navigation.state.params.userid);},[]);
   return (
     <ImageBackground source={WhiteBackgroundImage} style={style.backgroundImg}>
       <View style={style.align}>
         <View style={style.mainLogoWrapper}>
           <Image source={EditUserInfoImage} style={style.mainLogo}></Image>
         </View>
-        <View>
-          <Text style={style.aligntext}>아이디</Text>
-        </View>
         <View style={style.inputWrapper}>
-          <TextInput autoCompleteType='password' placeholder='비밀번호' style={style.Input}/>
-          <TextInput autoCompleteType='password' placeholder='비밀번호 재확인'style={style.Input}/>
-          <TextInput autoCompleteType='email' placeholder='Email 주소'style={style.Input}/>
+          <TextInput 
+            secureTextEntry={true}
+            autoCompleteType='password' 
+            placeholder='비밀번호' 
+            style={style.Input} 
+            onChange={
+              e => {
+                e.preventDefault();
+                setPassword(e.nativeEvent.text);
+              }
+            } />
+          <TextInput 
+            secureTextEntry={true}
+            autoCompleteType='password' 
+            placeholder='비밀번호 재확인'
+            style={style.Input}
+            onChange={e=>{
+              e.preventDefault();
+              setCheckPassword(e.nativeEvent.text);
+            }}
+          />
+          <TextInput 
+            autoCompleteType='email' 
+            placeholder='Email 주소'
+            style={style.Input}
+            onChange={e=>{
+              e.preventDefault();
+              setEmail(e.nativeEvent.text);
+            }}
+          />
         </View>
         <View style={style.buttonWrapper}>
-          <TouchableOpacity style={style.button} onPress={()=> props.navigation.navigate('Mypage')}><Text>수정하기</Text></TouchableOpacity>
+          <TouchableOpacity style={style.button} onPress={()=> changeHandler()}><Text>수정하기</Text></TouchableOpacity>
           <TouchableOpacity style={style.button}onPress={()=> props.navigation.goBack()} ><Text>뒤로가기</Text></TouchableOpacity>
         </View>
       </View>
